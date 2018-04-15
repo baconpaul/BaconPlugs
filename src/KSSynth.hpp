861e8744d64c319a45cfc1c8404fa96d272fe210
Paul Walker <paul@pwjw.com>
2018-04-14 21:24:32 -0400

    Unworking checkpointed KS port into C++

---
+++ src/KSSynth.hpp
@@ -0,0 +1,127 @@
+// An implementation of the KSSYnth class in the ipython notebook in ../pynb/KarplusStrong.ipynb
+
+#include <string>
+#include <vector>
+
+class KSSynth {
+public:
+  typedef enum InitPacket {
+    RANDOM,
+    SQUARE,
+    SAW,
+    NOISYSAW,
+    SIN,
+    SINCHIRP // if you add remember to fix the count below...
+  } InitPacket;
+
+  std::string initPacketName( InitPacket p )
+  {
+    switch(p)
+      {
+      case RANDOM: return "random";
+      case SQUARE: return "square";
+      case SAW: return "saw";
+      case NOISYSAW: return "noisysaw";
+      case SIN: return "sin";
+      case SINCHIRP: return "sinchirp";
+      }
+  }
+
+  int numInitPackets()
+  {
+    return (int)SINCHIRP + 1;
+  }
+
+  typedef enum FilterType {
+    WEIGHTED_ONE_SAMPLE
+  } FilterType;
+
+  std::string filterTypeName( FilterType p )
+  {
+    switch(p)
+      {
+      case WEIGHTED_ONE_SAMPLE: return "wgt 1samp";
+      }
+  }
+
+  int numFilterTypes()
+  {
+    return WEIGHTED_ONE_SAMPLE + 1;
+  }
+
+private:
+  // here's my interior state
+  InitPacket packet;
+  FilterType filter;
+
+  float filtParamA, filtParamB, filtParamC, filtAtten, filtAttenScaled;
+
+  float freq;
+  int burstLen;
+  int sampleRate;
+
+  long pos;
+  std::vector< float > delay;
+
+  float wfMin, wfMax;
+
+public:
+
+  KSSynth( int sampleRateIn, float minv, float maxv )
+    :
+    sampleRate( sampleRateIn ), wfMin( minv ), wfMax( maxv ),
+    packet( RANDOM ),
+    filter( WEIGHTED_ONE_SAMPLE ),
+    filtAtten( 3.0f ),
+    filtParamA( 0.5f ),
+    filtParamB( 0.0f ),
+    filtParamC( 0.0f ),
+    pos( 0 )
+  {
+    setFreq( 220 );
+  }
+
+  void setFreq( float f )
+  {
+    freq = f;
+    burstLen = (int)( ( sampleRate / freq + 0.5 ) * 2 );
+    filtAttenScaled = filtAtten / 100 / ( freq / 440 );
+    delay.resize( burstLen );
+  }
+
+  void trigger( float f )
+  {
+    setFreq( f );
+    switch( packet )
+      {
+      case RANDOM:
+        for( int i=0; i<burstLen; ++i )
+          delay[ i ] = rand();
+        break;
+      default:
+        // BLOW UP (so remove this later)
+        break;
+      }
+  }
+  
+  int step()
+  {
+    int dpos = pos % burstLen;
+    int dpnext = ( pos + 1 ) % burstLen;
+    int dpfill = ( pos - 1 ) % burstLen;
+    pos++;
+
+    float filtval;
+    switch( filter )
+      {
+      case WEIGHTED_ONE_SAMPLE:
+        float ftw = filtParamA;
+        float fta = filtAttenScaled;
+        filtval = ( ftw * delay[ dpos ] + ( 1.0 - ftw ) * delay[ dpos ] ) * ( 1.0 - fta );
+        break;
+      }
+    delay[ dpfill ] = filtval;
+    return (filtval - wfMin ) / ( wfMax - wfMin );
+  }
+  
+};
---
+++ standalone/kssynthDemo.cpp
@@ -0,0 +1,34 @@
+#include "../src/KSSynth.hpp"
+#include "standalone_helpers.hpp"
+
+#include <iostream>
+
+struct KSGen : StepHandler
+{
+  KSSynth s;
+  KSGen() : s( 44100, -1, 1 ) { }
+
+  virtual int dostep( void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
+                      double streamTime, RtAudioStreamStatus status ) override
+  {
+    unsigned int i, j;
+    double *buffer = (double *) outputBuffer;
+
+    for ( i=0; i<nBufferFrames; i++ ) {
+      *buffer++ = s.step();
+    }
+    return 0;
+    
+  }
+};
+
+int main( int argc, char **argv )
+{
+  KSGen gen;
+  
+  std::cout << "packets " << gen.s.numInitPackets() << "\n";
+
+  gen.s.trigger( 220 );
+  
+  gen.playAudioUntilEnterPressed();
+}
