62093f2f88b9b3f7fc7aed9f5d395e0f9fb31ebe
Paul Walker <paul@pwjw.com>
2018-06-02 18:37:10 -0400

    Test that at least the main clock is regular #11

--- standalone/test/PolyGnome.cpp
+++ standalone/test/PolyGnome.cpp
@@ -9,9 +9,37 @@ typedef PolyGnome< StandaloneModule > P;
 TEST_CASE( "Polygnome main clock beats regularly" )
 {
   P p;
+  p.inputs[ P::CLOCK_INPUT ].value = 0;
+  p.params[ P::CLOCK_PARAM ].value = 6.0f;
+  
   p.step();
 
-  REQUIRE( p.outputs[ P::CLOCK_GATE_0 ].value > 5 );
+  CHECK( p.outputs[ P::CLOCK_GATE_0 ].value > 5 );
+  std::vector< int > maxCounts;
+  for( int i=0; i<20; ++i )
+    {
+      int countOn = 0;
+
+      while( p.outputs[ P::CLOCK_GATE_0 ].value > 5 )
+        {
+          countOn ++;
+          p.step();
+        }
+
+      maxCounts.push_back( countOn );
+      
+      while( p.outputs[ P::CLOCK_GATE_0 ].value < 5 )
+        {
+          countOn --;
+          p.step();
+        }
+
+      CHECK( countOn <= 1);
+      CHECK( countOn >= -1 );
+    }
+
+  for( auto i=1; i<maxCounts.size(); ++i )
+    CHECK( abs( maxCounts[ i ] - maxCounts[ i - 1 ] ) <= 1 );
 };
 
 
