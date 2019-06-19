b78ae99603a6ec39fcd6b000c7f77ceb99f04cfd
Paul <paul@pwjw.com>
2019-06-19 11:28:15 -0400

    Move to rack sdk 1.0.0

--- scripts/release-notes.sh
+++ scripts/release-notes.sh
@@ -6,9 +6,10 @@ cat <<- EOH
 This release is automatically generated every time I push BaconMusic
 to master. As such it is the latest version of the code and may be
 unstable, unusable, unsuitable for human consumption, and so on.
+The stable release is always available in the Rack Plugin Manager.
 
 These assets were built against
-https://vcvrack.com/downloads/Rack-SDK-1.dev.b4bd09a.zip
+https://vcvrack.com/downloads/Rack-SDK-1.0.0.zip
 
 The build date and most recent commits are:
 
