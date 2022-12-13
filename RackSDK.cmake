8f68cc6e2b57944408268d74a30e7d12ee9a5260
Silvio Kunaschk <silvio.kunaschk@gmail.com>
2022-12-13 08:53:16 +0100

    fix issues from review

--- RackSDK.cmake
+++ RackSDK.cmake
@@ -46,8 +46,8 @@ add_compile_options(-Wall -Wextra -Wno-unused-parameter)
 if (${CMAKE_CXX_STANDARD} AND (${CMAKE_CXX_STANDARD} GREATER_EQUAL 11))
   message(STATUS "Retaining CMAKE_CXX_STANDARD=${CMAKE_CXX_STANDARD}")
 else()
-  message(STATUS "Defaulting CMAKE_CXX_STANDARD to 11")
   set(CMAKE_CXX_STANDARD 11)
+  message(STATUS "Defaulting CMAKE_CXX_STANDARD to ${CMAKE_CXX_STANDARD}")
 endif()
 
 add_library(${RACK_PLUGIN_LIB} MODULE)
@@ -68,10 +68,7 @@ if (${CMAKE_SYSTEM_NAME} MATCHES "Windows")
     message(FATAL_ERROR "Rack plugin development environment is only supported for MSYS2/MinGW")
   endif ()
   target_compile_definitions(RackSDK INTERFACE ARCH_WIN _USE_MATH_DEFINES)
-  # disabled - causes surge-rack/surge/libs/sst/sst-plugininfra/src/misc_windows.cpp:20:20: error: cannot convert 'const char*' to 'LPCWSTR' {aka 'const wchar_t*'}
-  # see also https://gcc.gnu.org/onlinedocs/gcc/x86-Windows-Options.html
-  #add_compile_options(-municode)
-  target_compile_options(RackSDK INTERFACE -Wsuggest-override)
+  target_compile_options(RackSDK INTERFACE -Wsuggest-override -municode)
 endif ()
 
 if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
