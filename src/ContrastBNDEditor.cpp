e7104e2442e0b69f200eaea196b03a15e3481a2c
Paul <baconpaul@users.noreply.github.com>
2023-01-03 13:52:26 -0500

    More contrast t2eaks (#95)
    1. Save contrast setting so it can be templated
    2. Add a back-to-default mode
    3. Tighten up some colors

--- src/ContrastBNDEditor.cpp
+++ src/ContrastBNDEditor.cpp
@@ -39,6 +39,26 @@ struct ContrastBNDEditor : virtual bp::BaconModule
     }
 
     void process(const ProcessArgs &args) override {}
+
+    enum ColorScheme
+    {
+        DEFAULT,
+        HICONTRAST_LIGHT,
+        HICONTRAST_DARK,
+    };
+
+    std::atomic<ColorScheme> colorScheme;
+    json_t *dataToJson() override
+    {
+        auto *rootJ = json_object();
+        json_object_set(rootJ, "colorScheme", json_integer(colorScheme));
+        return rootJ;
+    }
+    void dataFromJson(json_t *rootJ) override
+    {
+        auto cs = json_object_get(rootJ, "colorScheme");
+        colorScheme = (ColorScheme)(cs ? json_integer_value(cs) : 0);
+    }
 };
 
 struct ContrastBNDEditorWidget : bp::BaconModuleWidget
@@ -49,16 +69,27 @@ struct ContrastBNDEditorWidget : bp::BaconModuleWidget
     std::vector<std::string> deleteOnExit;
     ~ContrastBNDEditorWidget() {}
 
+    void bndDef() { rack::ui::setTheme(nvgRGB(0x33, 0x33, 0x33), nvgRGB(0xf0, 0xf0, 0xf0)); }
+
     void hiLight()
     {
         auto bt = bndGetTheme();
         auto q = *bt;
-        auto wl = 245;
-        q.menuItemTheme.textColor = nvgRGB(0, 0, 0);
-        q.menuTheme.textColor = nvgRGB(0, 0, 0);
-        q.menuTheme.innerColor = nvgRGB(wl, wl, wl);
-        q.tooltipTheme.innerSelectedColor = nvgRGB(wl, wl, wl);
-        q.tooltipTheme.innerColor = nvgRGB(wl, wl, wl);
+        auto wl = 0;
+        auto dl = 245;
+
+        q.menuItemTheme.textColor = nvgRGB(wl, wl, wl);
+        q.menuItemTheme.innerColor = nvgRGB(dl, dl, dl);
+        q.menuItemTheme.textSelectedColor = nvgRGB(dl, dl, dl);
+        q.menuItemTheme.innerSelectedColor = nvgRGB(wl, wl, wl);
+
+        q.menuTheme.textColor = nvgRGB(0, 0, 20);
+        q.menuTheme.innerColor = nvgRGB(dl, dl, dl);
+        q.menuTheme.textSelectedColor = nvgRGB(dl * 0.9, dl * 0.9, dl);
+
+        q.tooltipTheme.innerSelectedColor = nvgRGB(dl, dl, dl);
+        q.tooltipTheme.innerColor = nvgRGB(dl, dl, dl);
+
         bndSetTheme(q);
     }
     void hiDark()
@@ -68,8 +99,14 @@ struct ContrastBNDEditorWidget : bp::BaconModuleWidget
         auto wl = 245;
         auto dl = 5;
         q.menuItemTheme.textColor = nvgRGB(wl, wl, wl);
-        q.menuTheme.textColor = nvgRGB(wl, wl, wl);
+        q.menuItemTheme.innerColor = nvgRGB(dl, dl, dl);
+        q.menuItemTheme.textSelectedColor = nvgRGB(dl, dl, dl);
+        q.menuItemTheme.innerSelectedColor = nvgRGB(wl, wl, wl);
+
+        q.menuTheme.textColor = nvgRGB(wl * 0.95, wl * 0.95, wl);
         q.menuTheme.innerColor = nvgRGB(dl, dl, dl);
+        q.menuTheme.textSelectedColor = nvgRGB(dl, dl, 20);
+
         q.tooltipTheme.innerSelectedColor = nvgRGB(dl, dl, dl);
         q.tooltipTheme.innerColor = nvgRGB(dl, dl, dl);
         bndSetTheme(q);
@@ -77,10 +114,53 @@ struct ContrastBNDEditorWidget : bp::BaconModuleWidget
 
     void appendModuleSpecificContextMenu(Menu *menu) override
     {
-        menu->addChild(new rack::ui::MenuSeparator);
-        menu->addChild(rack::createMenuLabel("Menu Items"));
-        menu->addChild(rack::createMenuItem("High Contrast Light", "", [this]() { hiLight(); }));
-        menu->addChild(rack::createMenuItem("High Contrast Dark", "", [this]() { hiDark(); }));
+        auto c = dynamic_cast<ContrastBNDEditor *>(module);
+
+        if (c)
+        {
+            menu->addChild(new rack::ui::MenuSeparator);
+            menu->addChild(rack::createMenuLabel("Menu Items"));
+            menu->addChild(rack::createMenuItem(
+                "Default", CHECKMARK(c->colorScheme == ContrastBNDEditor::DEFAULT),
+                [c]() { c->colorScheme = ContrastBNDEditor::ColorScheme::DEFAULT; }));
+            menu->addChild(rack::createMenuItem(
+                "High Contrast Light",
+                CHECKMARK(c->colorScheme == ContrastBNDEditor::HICONTRAST_LIGHT),
+                [c]() { c->colorScheme = ContrastBNDEditor::ColorScheme::HICONTRAST_LIGHT; }));
+            menu->addChild(rack::createMenuItem(
+                "High Contrast Dark",
+                CHECKMARK(c->colorScheme == ContrastBNDEditor::HICONTRAST_DARK),
+                [c]() { c->colorScheme = ContrastBNDEditor::ColorScheme::HICONTRAST_DARK; }));
+        }
+    }
+
+    int lastScheme{-1};
+    void step() override
+    {
+        if (module)
+        {
+            auto c = dynamic_cast<ContrastBNDEditor *>(module);
+            if (c)
+            {
+                if (lastScheme != c->colorScheme)
+                {
+                    lastScheme = c->colorScheme;
+                    switch (c->colorScheme)
+                    {
+                    case ContrastBNDEditor::DEFAULT:
+                        bndDef();
+                        break;
+                    case ContrastBNDEditor::HICONTRAST_LIGHT:
+                        hiLight();
+                        break;
+                    case ContrastBNDEditor::HICONTRAST_DARK:
+                        hiDark();
+                        break;
+                    }
+                }
+            }
+        }
+        Widget::step();
     }
 };
 
